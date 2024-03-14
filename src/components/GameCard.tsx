5c5e187e5b9bf66400591075edc43384ac4bbdbd
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-03-14 01:11:07 +0100

    refactor: remove style duplication

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,27 +3,30 @@ import { Game } from '../hooks/useGames';
 import PlatformIconsList from './PlatformIconsList';
 import CriticScore from './CriticScore';
 import cropImage from '../services/image-url';
+import GameCardContainer from './GameCardContainer';
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
-      <Image
-        objectFit="cover"
-        src={cropImage(game.background_image, 600, 400)}
-      />
-      <CardBody>
-        <Heading fontSize="xl">{game.name}</Heading>
-        <HStack justifyContent={'space-between'}>
-          <PlatformIconsList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image
+          objectFit="cover"
+          src={cropImage(game.background_image, 600, 400)}
+        />
+        <CardBody>
+          <Heading fontSize="xl">{game.name}</Heading>
+          <HStack justifyContent={'space-between'}>
+            <PlatformIconsList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
