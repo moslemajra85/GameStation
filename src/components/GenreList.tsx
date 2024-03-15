85733ab87d48ec7ece55d54e9cffb8ff42d6a7d6
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-03-15 03:38:07 +0100

    highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,10 +9,11 @@ import {
 import useGenres, { Genre } from '../hooks/useGenres';
 
 interface Props {
-  onSelectedGenre: (genre: Genre) => void
+  onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectedGenre}: Props) => {
+const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -31,6 +32,7 @@ const GenreList = ({onSelectedGenre}: Props) => {
               onClick={() => onSelectedGenre(genre)}
               fontSize="lg"
               variant="link"
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
             >
               {genre.name}
             </Button>
